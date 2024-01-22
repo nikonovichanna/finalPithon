10476544b43b7a48e23f0b96dab4a88cd39250ac
nikonovichanna <nikonovicvhhanna@gmail.com>
2024-01-22 21:38:20 +0300

    Доработка с учетом возможности просмотреть заметку

--- notes.py
+++ notes.py
@@ -13,17 +13,20 @@ def save_notes(notes):
     with open('notes.json', 'w', encoding='utf-8') as file:
         json.dump(notes, file, indent=4, ensure_ascii=False)
 
+def display_note_details(note):
+    print(f"ID: {note['id']}")
+    print(f"Заголовок: {note['title']}")
+    print(f"Тело заметки: {note['body']}")
+    print(f"Дата создания: {note['created_at']}")
+    print(f"Дата изменения: {note['updated_at']}")
+    print("==============================")
+
 def display_notes(notes):
     if not notes:
         print("Список заметок пуст.")
     else:
         for note in notes:
-            print(f"ID: {note['id']}")
-            print(f"Заголовок: {note['title']}")
-            print(f"Тело заметки: {note['body']}")
-            print(f"Дата создания: {note['created_at']}")
-            print(f"Дата изменения: {note['updated_at']}")
-            print("==============================")
+            display_note_details(note)
 
 def add_note():
     notes = load_notes()
@@ -87,7 +90,16 @@ def search_notes_by_date():
             print("Текст:", note['body'])
             print()
     else:
-        print("Заметки, отредактированные", search_date, "не найдены.")    
+        print("Заметки, отредактированные", search_date, "не найдены.")  
+
+def view_note():
+    notes = load_notes()
+    id = int(input("Введите ID заметки для просмотра: "))
+    for note in notes:
+        if note['id'] == id:
+            display_note_details(note)
+            return
+    print("Заметка с указанным ID не найдена.")  
 
 
 def main_menu():
@@ -98,7 +110,8 @@ def main_menu():
         print("3. Редактировать заметку")
         print("4. Удалить заметку")
         print("5. Искать заметки по дате")
-        print("6. Выйти из приложения")
+        print("6. Просмотреть заметку")
+        print("7. Выйти из приложения")
         print("==============================")
         choice = input("Введите номер действия: ")
         if choice == '1':
@@ -111,8 +124,10 @@ def main_menu():
         elif choice == '4':
             delete_note()
         elif choice == '5':
-            search_notes_by_date()    
+            search_notes_by_date()
         elif choice == '6':
+            view_note()    
+        elif choice == '7':
             break
         else:
             print("Некорректный выбор.")
